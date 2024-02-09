b733a9953dfc6a62ad9631620f549fdf8316c160
Julien Abbadie <julien.abbadie@pytheascapital.com>
2024-02-09 14:30:47 +0100

    init

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,10 +15,10 @@ export default function Navbar() {
   return (
     <nav
       className={
-        "w-full sticky bg-gray-900 top-0 z-40 flex shadow-zinc-800  shadow p-5"
+        "w-full sticky bg-gray-900 justify-center align-middle top-0 z-40 flex shadow-zinc-800  shadow p-5"
       }
     >
-      <ul className={"w-full flex justify-around"}>
+      <ul className={"w-full max-w-7xl flex justify-between"}>
         {navLinks.map((link) => (
           <li key={link.path}>
             <Link to={link.path}>{link.title}</Link>
